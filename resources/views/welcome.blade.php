48ce2e6c03a77ea590830e0348a4ea7b0951bf50
Amresh-Sin <amresh65516@gmail.com>
2024-07-16 11:10:43 -0700

    amresh update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Captain work flow
+                    Amresh
                 </div>
 
                 <div class="links">
