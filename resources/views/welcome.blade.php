b798f6f1ed3bfad3984426f00beb4ffbed5b01bf
Niraj-Kuma <singhniraj.2063@gmail.com>
2024-07-17 23:08:27 +0545

    work from Laptop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Captain work Flow
+                    6th semester project work
                 </div>
 
                 <div class="links">
